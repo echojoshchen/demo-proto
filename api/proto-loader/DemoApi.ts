a49df5c10aa64560c8a276195ecb56722ebaa4d1
echojoshchen <echojoshchen@gmail.com>
2022-02-11 22:17:28 -0500

    add grpc-node examples

--- api/proto-loader/DemoApi.ts
+++ api/proto-loader/DemoApi.ts
@@ -5,14 +5,14 @@ import type { MethodDefinition } from '@grpc/proto-loader'
 import type { DemoContainer as _DemoContainer, DemoContainer__Output as _DemoContainer__Output } from './DemoContainer';
 
 export interface DemoApiClient extends grpc.Client {
-  DoSomething(argument: _DemoContainer, metadata: grpc.Metadata, options: grpc.CallOptions, callback: (error?: grpc.ServiceError, result?: _DemoContainer__Output) => void): grpc.ClientUnaryCall;
-  DoSomething(argument: _DemoContainer, metadata: grpc.Metadata, callback: (error?: grpc.ServiceError, result?: _DemoContainer__Output) => void): grpc.ClientUnaryCall;
-  DoSomething(argument: _DemoContainer, options: grpc.CallOptions, callback: (error?: grpc.ServiceError, result?: _DemoContainer__Output) => void): grpc.ClientUnaryCall;
-  DoSomething(argument: _DemoContainer, callback: (error?: grpc.ServiceError, result?: _DemoContainer__Output) => void): grpc.ClientUnaryCall;
-  doSomething(argument: _DemoContainer, metadata: grpc.Metadata, options: grpc.CallOptions, callback: (error?: grpc.ServiceError, result?: _DemoContainer__Output) => void): grpc.ClientUnaryCall;
-  doSomething(argument: _DemoContainer, metadata: grpc.Metadata, callback: (error?: grpc.ServiceError, result?: _DemoContainer__Output) => void): grpc.ClientUnaryCall;
-  doSomething(argument: _DemoContainer, options: grpc.CallOptions, callback: (error?: grpc.ServiceError, result?: _DemoContainer__Output) => void): grpc.ClientUnaryCall;
-  doSomething(argument: _DemoContainer, callback: (error?: grpc.ServiceError, result?: _DemoContainer__Output) => void): grpc.ClientUnaryCall;
+  DoSomething(argument: _DemoContainer, metadata: grpc.Metadata, options: grpc.CallOptions, callback: grpc.requestCallback<_DemoContainer__Output>): grpc.ClientUnaryCall;
+  DoSomething(argument: _DemoContainer, metadata: grpc.Metadata, callback: grpc.requestCallback<_DemoContainer__Output>): grpc.ClientUnaryCall;
+  DoSomething(argument: _DemoContainer, options: grpc.CallOptions, callback: grpc.requestCallback<_DemoContainer__Output>): grpc.ClientUnaryCall;
+  DoSomething(argument: _DemoContainer, callback: grpc.requestCallback<_DemoContainer__Output>): grpc.ClientUnaryCall;
+  doSomething(argument: _DemoContainer, metadata: grpc.Metadata, options: grpc.CallOptions, callback: grpc.requestCallback<_DemoContainer__Output>): grpc.ClientUnaryCall;
+  doSomething(argument: _DemoContainer, metadata: grpc.Metadata, callback: grpc.requestCallback<_DemoContainer__Output>): grpc.ClientUnaryCall;
+  doSomething(argument: _DemoContainer, options: grpc.CallOptions, callback: grpc.requestCallback<_DemoContainer__Output>): grpc.ClientUnaryCall;
+  doSomething(argument: _DemoContainer, callback: grpc.requestCallback<_DemoContainer__Output>): grpc.ClientUnaryCall;
   
 }
 
